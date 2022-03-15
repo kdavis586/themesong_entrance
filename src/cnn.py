5a7588b364126cfb17f5389cb8801f2e6f9fefea
Kaelan Davis <kaelandavis01@gmail.com>
2022-03-15 00:16:18 -0500

    Following image recognition tutorial on tensorflow

--- src/cnn.py
+++ src/cnn.py
@@ -4,11 +4,23 @@ Convolution Neural Network (CNN) for image recognition.
 This module contains all logic for dataset preprocessing (image preprocessing,
 creating training csv), CNN training, and CNN classification.
 """
-import pandas as pd
-import numpy as np
 import csv
+import data_collect
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+# Default tensorflow params
+DEFAULT_BATCH_SIZE = 32
+DEFAULT_IMG_WIDTH = data_collect.DEFAULT_RES_WIDTH
+DEFAULT_IMG_HEIGHT = data_collect.DEFAULT_RES_HEIGHT
+DEFAULT_EPOCH_AMOUNT = 15
+
 
 
 def _create_cnn_csv(path: str, csv_name: str = "labeled_data"):
@@ -50,5 +62,98 @@ def _create_cnn_csv(path: str, csv_name: str = "labeled_data"):
                         # dirname will serve as the label for all files in it
                         writer.writerow([filepath, dirname])
 
-def something():
-    print("This is garbage...")
+def create_tensorflow_dataset(dataset_dir):
+    # Creating training and validation datasets
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(DEFAULT_IMG_HEIGHT, DEFAULT_IMG_WIDTH),
+        batch_size=DEFAULT_BATCH_SIZE)
+
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(DEFAULT_IMG_HEIGHT, DEFAULT_IMG_WIDTH),
+        batch_size=DEFAULT_BATCH_SIZE)
+    
+    class_names = train_ds.class_names
+    
+    # Configuring Dataset for performance
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    # Standardizing the dataset
+    normalization_layer = layers.Rescaling(1./255)
+
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds)) # do I need this???
+
+    num_classes = len(class_names)
+
+    # Use data augmentation to expose model to more samples to reduce overfitting
+    data_augmentation = keras.Sequential(
+        [
+            layers.RandomFlip("horizontal",
+                            input_shape=(DEFAULT_IMG_HEIGHT,
+                                        DEFAULT_IMG_WIDTH,
+                                        3)),
+            layers.RandomRotation(0.1),
+            layers.RandomZoom(0.1),
+        ]
+    )
+
+    # Creating the model
+    model = Sequential([
+        data_augmentation,
+        layers.Rescaling(1./255, input_shape=(DEFAULT_IMG_HEIGHT, DEFAULT_IMG_WIDTH, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2), # Use dropout to reduce overfitting
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    # Compiling the model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    # Training the model
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=DEFAULT_EPOCH_AMOUNT
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(DEFAULT_EPOCH_AMOUNT)
+    
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    return plt.show()
--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+#%%
 from data_collect import create_dataset
+from cnn import create_tensorflow_dataset
+import os
 
-create_dataset()
+import matplotlib
+import matplotlib.pyplot as plt
+
+DATASET_DIR = os.path.join(os.getcwd(), "datasets")
+
+#create_dataset()
+create_tensorflow_dataset(DATASET_DIR)
+# %%
